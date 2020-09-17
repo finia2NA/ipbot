c24a5c8a2e49e6a83aefa0322fda9a9300b8d7a8
finia2na <undifini@gmail.com>
2020-09-18 00:55:40 +0200

    store last ip to file

--- ipbot.py
+++ ipbot.py
@@ -7,6 +7,18 @@ import miniupnpc
 
 client = discord.Client()
 
+def storeLastIP(lastip):
+  with open(".lastip", "w") as f:
+    f.write(lastip)
+    f.close()
+
+def getLastIP():
+  with open(".lastip", "r") as f:
+    re = f.readlines()
+    f.close
+  return re
+
+
 def getConfig():
   try:
     with open("config.json", 'r') as j:
@@ -14,40 +26,49 @@ def getConfig():
   except FileNotFoundError:
     FileNotFoundError: "Please provide a config.json"
 
-def getIP():
-  u = miniupnpc.UPnP()
-  u.discoverdelay = 200
-  u.discover()
-  u.selectigd()
-  return u.externalipaddress()
+def ipChanged():
+  try:
+    u = miniupnpc.UPnP()
+    u.discoverdelay = 200
+    u.discover()
+    u.selectigd() 
+    currentip = u.externalipaddress()
+    if getLastIP() != currentip:
+      return (True, currentip)
+    else:
+      return (False, currentip)
+  except Exception:
+    print("ip lookup was unsuccessfull.")
+    return (False, getLastIP)
+
+  
 
 async def eventloop():
   await client.wait_until_ready()
 
-  # channels = getConfig()["channels"]
-  # for identifier in channels:
-  #   channel = client.get_channel(identifier)
-  #   await channel.send('New IP:')
-
   # endlosschleife inc (TM)
-
-  lastIP = None
   while(True):
-    if getIP() != lastIP:
-      lastIP = getIP()
+    ipResult = ipChanged()
+
+    if ipResult[0]:
+      storeLastIP(ipResult[1])
 
       channels = getConfig()["channels"]
 
       for identifier in channels:
         channel = client.get_channel(identifier)
-        await channel.send('New IP: `' + lastIP + '`')
-    print("ip changed to " + lastIP + ",   messages have been sent")
+        await channel.send('New IP: `' + ipResult[1] + '`')
+    print("ip changed to " + ipResult[1] + ",   messages have been sent")
     sleep(120)
   
   
 
 TOKEN = getConfig()["token"]
 print("ipbot: starting")
+
+# create .lastip if not exists
+with open(".lastip", "w+") as f:
+  f.close()
 
 client.loop.create_task(eventloop())
 
